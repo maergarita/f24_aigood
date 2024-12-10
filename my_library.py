5bb8c4d64201642f9933622c984af0da6f4fe116
maergarita <maerg@uoregon.edu>
2024-12-09 22:33:00 -0800

    Update my_library.py

--- my_library.py
+++ my_library.py
@@ -129,6 +129,31 @@ def run_random_forest(train, test, target, n):
 
 
 
+def try_archs(train, test, target_column_name, architectures, thresholds):
+  arch_acc_dict = {}  #ignore if not attempting extra credit
+
+  #now loop through architectures
+for arch in all_architectures:
+  probs = up_neural_net(scaled_train, scaled_test, arch, target)
+
+  pos_probs = [pos for neg,pos in probs]
+
+    #loop through thresholds
+  all_mets = []
+  for t in thresholds:
+    predictions = [1 if pos>=t else 0 for pos in pos_probs]
+    pred_act_list = up_zip_lists(predictions, up_get_column(scaled_test, target))
+    mets = metrics(pred_act_list)
+    mets['Threshold'] = t
+    all_mets = all_mets + [mets]
+
+    #arch_acc_dict[tuple(arch)] = max(...)  #extra credit - uncomment if want to attempt
+
+  print(f'Architecture: {arch}')
+  display(up_metrics_table(all_mets))
+
+
+
 
 
 
